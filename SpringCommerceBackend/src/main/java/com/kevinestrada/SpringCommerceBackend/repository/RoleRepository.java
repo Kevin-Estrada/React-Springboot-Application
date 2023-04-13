ce26a722cf5dca73d82931b56a96dd217fcb84f0
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2023-04-13 15:22:55 -0500

    Adding the basic repository interfaces for both User and Role

--- SpringCommerceBackend/src/main/java/com/kevinestrada/SpringCommerceBackend/repository/RoleRepository.java
+++ SpringCommerceBackend/src/main/java/com/kevinestrada/SpringCommerceBackend/repository/RoleRepository.java
@@ -1,5 +1,14 @@
 package com.kevinestrada.SpringCommerceBackend.repository;
 
-public class RoleRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kevinestrada.SpringCommerceBackend.models.ERole;
+import com.kevinestrada.SpringCommerceBackend.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
